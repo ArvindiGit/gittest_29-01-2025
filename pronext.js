8dbf83bdda291139444334fe742f4d58001d31c7
ArvindiGit <arvindprajapati2569@gmail.com>
2025-01-29 22:15:32 +0530

    test thired now 3

--- pronext.js
+++ pronext.js
@@ -1,6 +1,6 @@
 const pronext = () => {
   return (
-    <div>pronext</div>
+    <div>pronext Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti dolor doloribus animi reprehenderit voluptas, nam officia nisi ipsum tempora dolorem. Quod, libero rerum! Corporis in quod, optio quaerat quos harum.</div>
   )
 }
 export default pronext
